b6e662b356cee63796c6d13b3a7f34837c95e7c3
daonghia2404 <daonghia2404@gmail.com>
2021-01-20 17:14:10 +0700

    Config Eslint and Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,16 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ["plugin:vue/essential", "eslint:recommended", 'prettier'],
   parserOptions: {
     parser: "babel-eslint"
   },
-  plugins: ['prettier'],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
     "semi": [0, "warn"],
     "quotes": [2, "single"],
     "eol-last": "warn",
-  }
+  },
+  plugins: ['prettier'],
 };
---
+++ .prettierrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  semi: false,
+  arrowParens: "always",
+  singleQuote: true,
+  endOfLine: "lf"
+}
